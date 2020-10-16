fab0d2543dd53cbaa7e58316771e6f443fa57d0f
Moniqueemh <mohernandez9992@yahoo.com>
2020-10-15 18:29:18 -0700

    added inquirer, prompt

--- index.js
+++ index.js
@@ -1,10 +1,64 @@
-// array of questions for user
-const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
+// array of questions for user
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Name of title?",
+        name: "Title",
+    },
+    {
+        type: "input",
+        message: "What is your project about?",
+        name: "Description",
+    },
+    {
+        type: "input",
+        message: "What do you have to install for this project?",
+        name: "Installation"
+    },
+    {
+        type: "input",
+        message: "",
+        name: "Usage",
+    },
+    {
+        type: "input",
+        message: "",
+        name: "Contributing",
+    },
+    {
+        type: "input",
+        message: "",
+        name: "Tests",
+    },
+    {
+        type: "input",
+        message: "",
+        name: "",
+    },
+    {
+        type: "input",
+        message: "What is your Github?",
+        name: "Username",
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "Email",
+    },
+])
+    .then(function (answers) {
+        console.log(answers);
+    })
+
 
 // function to write README file
-function writeToFile(fileName, data) {
+function generateReadMe(answers) {
 }
 
 // function to initialize program
